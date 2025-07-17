1e6213a40bb99600911567082431102ee1049b03
mac <mac@macs-MacBook-Pro.local>
2025-07-17 16:12:10 +0400

    Update project files

---
+++ src/event.ts
@@ -0,0 +1,44 @@
+// data/events.ts
+
+export const eventsD = {
+  Upcoming: [
+    {
+      title: "Quantum Meditation",
+      date: "12 september 10:00 Am",
+      location: "Τετ 13 Νοε Ethera Wellness and Retreat Center",
+      description:
+        "Weekly Meditations in the EESystem Lounge in Chania with Vivian Karavanou",
+      image: "/events/event1.jpg",
+      slug: "quantum-meditation-1",
+    },
+    {
+      title: "Quantum Meditation",
+      date: "12/13/14 september 10:00 Am",
+      location: "Τετ 13 Νοε Ethera Wellness and Retreat Center",
+      description:
+        "Weekly Meditations in the EESystem Lounge in Chania with Vivian Karavanou",
+      image: "/events/event2.jpg",
+      slug: "quantum-meditation-2",
+    },
+  ],
+  Past: [
+    {
+      title: "SCHUMANN'S RESONANCE",
+      date: "Past Event",
+      location: "Ethera Wellness and Retreat Center",
+      description:
+        "This brings our bodies back to a natural healthy state by tuning into the Earth's natural frequency. In essence it connects us to the Earth's natural heartbeat!",
+      image: "/events/event3.jpeg",
+      slug: "schumanns-resonance-1",
+    },
+    {
+      title: "SCHUMANN'S RESONANCE",
+      date: "Past Event",
+      location: "Ethera Wellness and Retreat Center",
+      description:
+        "This brings our bodies back to a natural healthy state by tuning into the Earth's natural frequency. In essence it connects us to the Earth's natural heartbeat!",
+      image: "/events/event4.png",
+      slug: "schumanns-resonance-2",
+    },
+  ],
+};
